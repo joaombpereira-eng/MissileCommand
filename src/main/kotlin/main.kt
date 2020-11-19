a3196f316fdc537b7899bac98a84806807fa8c49
João Pereira <joaopereira.eng.tec@gmail.com>
2020-11-19 12:15:34 +0000

    Adds code (fixed) written during session 11

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,30 +1,26 @@
 import pt.isel.canvas.*
 
+const val MARGIN = 50
+
 fun main() {
 
     onStart {
-        val canvas = Canvas(800,600, BLACK)
+        val canvas = Canvas(WORLD_WIDTH, WORLD_HEIGHT, BLACK)
 
-        var world = World(
-                Missile(
-                        Location(100.0, 0.0),
-                        Location(200.0, 200.0),
-                        Velocity(0.0,0.0),
-                        RED),
-                Explosion(
-                        Location(150.0, 150.0),
-                        5.0,
-                        1.03,
-                        RED)
-        )
+        val entry = Location((MARGIN..WORLD_WIDTH - MARGIN).random().toDouble(),0.0)
+        val target = Location((MARGIN..WORLD_WIDTH - MARGIN).random().toDouble(), WORLD_HEIGHT.toDouble())
+
+        var world = World(Missile(entry, entry, computeVelocity(entry, target)))
 
         canvas.onMouseDown {
-            // Always the mouse is pressed
-            world = TODO()
+            world = World(
+                    world.missile,
+                    Explosion(
+                            Location(it.x.toDouble(), it.y.toDouble()))
+            )
         }
 
         canvas.onTimeProgress(25) {
-            // Apply time passing to the world
             world = doStep(world)
             drawWorld(canvas, world)
         }
--- src/main/kotlin/world.kt
+++ src/main/kotlin/world.kt
@@ -1,7 +1,13 @@
+const val WORLD_WIDTH = 800
+const val WORLD_HEIGHT = 600
+
 /**
  * Representation of the game world
  */
-data class World(val missile: Missile, val explosion: Explosion)
+data class World(
+        val missile: Missile,
+        val explosion: Explosion? = null
+)
 
 /**
  * Computes new world based on the given one
@@ -9,12 +15,26 @@ data class World(val missile: Missile, val explosion: Explosion)
  * @param   world   The current [World] instance
  * @return  The new [World] instance
  */
-fun doStep(world: World) = World(
-        world.missile,
-        Explosion(
-                world.explosion.center,
-                world.explosion.radius * world.explosion.rate,
-                world.explosion.rate,
-                world.explosion.color
-        )
-)
+fun doStep(world: World): World {
+
+    val newExplosion: Explosion? = when {
+        world.explosion == null -> null
+        world.explosion.rate == GROWTH_RATE -> expandUntil(world.explosion, MAX_RADIUS)
+        else -> contractUntilZero(world.explosion)
+    }
+
+    val movedMissile = Missile(
+            world.missile.start,
+            move(world.missile.current, world.missile.velocity),
+            world.missile.velocity
+    )
+
+    return World(
+            movedMissile,
+            when {
+                newExplosion == null -> null
+                newExplosion != world.explosion -> newExplosion
+                else -> Explosion(newExplosion.center, newExplosion.radius, SHRINK_RATE, newExplosion.color)
+            }
+    )
+}

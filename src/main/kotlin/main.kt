b9731388269b64eccec8bd049faea9f20e2bcaad
João Pereira <joaopereira.eng.tec@gmail.com>
2020-11-30 15:40:54 +0000

    Adds solution with world containing several missiles

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,7 +6,12 @@ fun main() {
 
     onStart {
         val canvas = Canvas(WORLD_WIDTH, WORLD_HEIGHT, BLACK)
-        var world = World(createMissile(WORLD_WIDTH, WORLD_HEIGHT, MARGIN))
+        val missiles: List<Missile> = listOf(
+                createMissile(WORLD_WIDTH, WORLD_HEIGHT, MARGIN),
+                createMissile(WORLD_WIDTH, WORLD_HEIGHT, MARGIN),
+                createMissile(WORLD_WIDTH, WORLD_HEIGHT, MARGIN)
+        )
+        var world = World(missiles)
 
         canvas.onMouseDown {
             world = addExplosionToWorld(world, Location(it.x.toDouble(), it.y.toDouble()))
